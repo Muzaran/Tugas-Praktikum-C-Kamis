c6e816e4570b75bbe70a8b8564e735f250437976
M. Zaky Nugraha A R <144871095+Muzaran@users.noreply.github.com>
2024-01-08 17:47:26 +0700

    Add files via upload

---
+++ Pertemuan 1/tugas_mandiri_1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    char nama[] = "M. Zaky Nugraha A R";
+    int nim = 2143013;
+    char jurusan[] = "Program Studi DIII Informatika";
+    int umur = 21;
+    int tinggi = 172;
+   
+    printf("Nama Saya adalah %s.\n", nama);
+    printf("Dengan NIM %d.\n", nim);
+    printf("Saya Mengambil Jurusan %s.\n", jurusan);
+    printf("Umur Saya %d Tahun.\n", umur);
+    printf("Tinggi Saya %d cm.\n", tinggi);
+
+    return 0;
+}
---
+++ Pertemuan 1/tugas_mandiri_2.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    
+    int panjang = 10;
+    int lebar = 7;
+
+    // Algoritma Penjumlahan
+    int luas = panjang * lebar;
+
+  
+    printf("Panjang : %d cm\n", panjang);
+    printf("Lebar   : %d cm\n", lebar);
+    printf("Luas Persegi Panjang   : %d cm^2\n", luas);
+
+    return 0;
+}
