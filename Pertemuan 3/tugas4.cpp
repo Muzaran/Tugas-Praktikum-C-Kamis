5bcaf4feb1dc6cd413e2c37306011ea5c640be34
M. Zaky Nugraha A R <144871095+Muzaran@users.noreply.github.com>
2024-01-08 17:51:21 +0700

    Add files via upload

---
+++ Pertemuan 3/tugas4.cpp
@@ -0,0 +1,3 @@
+//Buatlah sebuah program Bahasa C untuk menginput sebuah nilai bilangan bulat 
+//positif, lalu tampilkan keterangan “GANJIL” jika bilangan tersebut adalah ganjil
+//dan “GENAP” jika bilangan tersebut adalah genap.
